c93680967c592b750e24325a11c2aa2280825d5c
ysingh57 <115952883+ysingh57@users.noreply.github.com>
2022-10-17 22:31:30 -0400

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python3 
+
+def finding_age():
+
+Year_of_Birth = input("Enter your year of birth: ")
+age = 2022-int (Year_of_Birth)
+return f"Your present age is (age) years"
+
+
+print(finding_age())
